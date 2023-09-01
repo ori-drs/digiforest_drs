e9c55a62329bcaa8d3cce35745f636086df3b423
Benoit Casseau <benoit@oxfordrobotics.institute>
2023-09-01 11:26:07 +0100

    remove unused code

--- digiforest_registration/src/digiforest_registration/tasks/horizontal_alignment.py
+++ digiforest_registration/src/digiforest_registration/tasks/horizontal_alignment.py
@@ -68,23 +68,12 @@ class HorizontalRegistration:
             image[y, x] = [max(v, image[y, x])]
             index += 1
 
-        # cv2.imshow("Image", image)
-        # cv2.waitKey(0)
-        # cv2.destroyAllWindows()
         return image
 
-    def filter_image(self, image, name="image.png"):
-        grayscale_image = (image * 255).astype(np.uint8)
-
-        # Convert to binary image
-        # _, binary_image = cv2.threshold(grayscale_image, 1, 255, cv2.THRESH_BINARY)
-
-        return grayscale_image
-
     def find_feature_matches(self, img1, img2):
-        # Initiate SIFT detector
+        # Initiate detector
         orb = cv2.ORB_create()
-        # find the keypoints and descriptors with SIFT
+        # find the keypoints and descriptors
         kp1, des1 = orb.detectAndCompute(img1, None)
         kp2, des2 = orb.detectAndCompute(img2, None)
 
@@ -104,49 +93,6 @@ class HorizontalRegistration:
             None,
             flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS,
         )
-
-        # cv2.imshow("Image", img3)
-        # cv2.waitKey(0)
-        # cv2.destroyAllWindows()
-
-    def find_local_maxima(self, img):
-        def distance_to_border(img, y, x):
-            """Distance from to the nearest border"""
-            return min(y, img.shape[0] - y, x, img.shape[1] - x)
-
-        # the input image is a float image
-        # find local maxima in the image
-        kernel_size = (25, 25)
-        kernel = np.ones(kernel_size, np.uint8)
-        dilated_img = cv2.dilate(img, kernel)
-        mask = cv2.compare(
-            img, dilated_img, cv2.CMP_GE
-        )  # set mask to 255 where img >= mask
-
-        white_pixel_indices = np.where(mask == 255)
-        white_points_coordinates = list(
-            zip(white_pixel_indices[1], white_pixel_indices[0])
-        )  # x, y coordinates
-
-        #
-        grayscale_image = (img * 255).astype(np.uint8)
-        maxima_img = cv2.cvtColor(grayscale_image, cv2.COLOR_GRAY2RGB)
-
-        valid_points = []
-        threshold_distance_to_border = 20
-        for point in white_points_coordinates:
-            if (
-                grayscale_image[point[1], point[0]] > 0
-                and distance_to_border(grayscale_image, point[1], point[0])
-                > threshold_distance_to_border
-            ):
-                cv2.circle(maxima_img, point, 3, (0, 0, 255), -1)
-                valid_points.append([point[0], point[1], 0])
-
-        cv2.imshow("Image", maxima_img)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
-        return np.array(valid_points)
 
     def draw_registration_result(self, source, target, transformation):
         source_temp = copy.deepcopy(source)
@@ -200,6 +146,6 @@ class HorizontalRegistration:
         proc = HeightImage()
         _ = proc.find_local_maxima(image)
 
-        _ = self.find_local_maxima(reference_image)
+        _ = proc.find_local_maxima(reference_image)
 
         # self.registration(center_pts1, center_pts2)
