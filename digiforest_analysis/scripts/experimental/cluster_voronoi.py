10da87dfee3e30cbaf2324757ddd34afb0430ae7
Leonard Freissmuth <l.freissmuth@gmail.com>
2024-02-25 12:35:01 +0000

    added evaluations

--- digiforest_analysis/scripts/experimental/cluster_voronoi.py
+++ digiforest_analysis/scripts/experimental/cluster_voronoi.py
@@ -1,4 +1,6 @@
+from typing import List
 from digiforest_analysis.tasks.terrain_fitting import TerrainFitting
+from digiforest_analysis.tasks.tree_reconstruction import Tree
 
 from digiforest_analysis.tasks.tree_segmentation_voronoi import TreeSegmentationVoronoi
 from digiforest_analysis.utils.io import load
@@ -7,61 +9,17 @@ from digiforest_analysis.utils.meshing import meshgrid_to_mesh
 
 import numpy as np
 import open3d as o3d
-from matplotlib import pyplot as plt
 import trimesh
 
 
 timer = Timer()
 
-
-def plot_mesh(vertices, triangles, points):
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection="3d")
-
-    # Extract x, y, z coordinates from vertices
-    x, y, z = zip(*vertices)
-
-    # Plot vertices
-    ax.scatter(x, y, z, c="r", marker="o")
-    ax.scatter(points[:, 0], points[:, 1], points[:, 2], c="b", marker="o", s=0.1)
-
-    # Plot triangles
-    for triangle in triangles:
-        ax.plot(
-            [vertices[triangle[0]][0], vertices[triangle[1]][0]],
-            [vertices[triangle[0]][1], vertices[triangle[1]][1]],
-            [vertices[triangle[0]][2], vertices[triangle[1]][2]],
-            "b-",
-        )
-
-        ax.plot(
-            [vertices[triangle[1]][0], vertices[triangle[2]][0]],
-            [vertices[triangle[1]][1], vertices[triangle[2]][1]],
-            [vertices[triangle[1]][2], vertices[triangle[2]][2]],
-            "b-",
-        )
-
-        ax.plot(
-            [vertices[triangle[2]][0], vertices[triangle[0]][0]],
-            [vertices[triangle[2]][1], vertices[triangle[0]][1]],
-            [vertices[triangle[2]][2], vertices[triangle[0]][2]],
-            "b-",
-        )
-
-    ax.set_xlabel("X")
-    ax.set_ylabel("Y")
-    ax.set_zlabel("Z")
-    xlim = ax.get_xlim3d()
-    ylim = ax.get_ylim3d()
-    zlim = ax.get_zlim3d()
-    ax.set_box_aspect((xlim[1] - xlim[0], ylim[1] - ylim[0], zlim[1] - zlim[0]))
-    plt.show()
-
-
 if __name__ == "__main__":
     # pcd_file = "/home/ori/logs/logs_evo_finland/exp16/combined_cloud.pcd"
-    pcd_file = "/home/ori/git/digiforest_drs/payload_clouds/pc021.pcd"
+    # pcd_file = "/home/ori/git/digiforest_drs/payload_clouds/pc021.pcd"
     # pcd_file = "/home/ori/logs/logs_evo_finland/exp01/2023-05-01-14-01-05-exp01/payload_clouds/cloud_1682946124_761436000.pcd"
+    pcd_file = "/home/ori/logs/2024-02-19-anymal-dolly-forest-of-dean/slam_outputs/2024-02-19-14-41-35-prior-map/combined_cloud.pcd"
+    debug_level = 0
     cloud, header = load(pcd_file, binary=True)
     if "VIEWPOINT" in header:
         header_data = [float(x) for x in header["VIEWPOINT"]]
@@ -77,8 +35,18 @@ if __name__ == "__main__":
     verts, tris = meshgrid_to_mesh(terrain)
     tm = trimesh.Trimesh(vertices=verts, faces=tris)
 
+    import trimesh
+
+    terrain_mesh = trimesh.Trimesh(vertices=verts, faces=tris)
+    terrain_mesh.fix_normals()
+    terrain_mesh.export(
+        "/home/ori/git/digiforest_drs/digiforest_analysis_ros/output/trees/logs/raw/trees/terrain_mesh.obj"
+    )
+
     # SEGMENT TREES
-    tree_seg = TreeSegmentationVoronoi(debug_level=2, clustering_method="voronoi")
+    tree_seg = TreeSegmentationVoronoi(
+        debug_level=debug_level, clustering_method="voronoi"
+    )
     clusters = tree_seg.process(
         cloud=cloud,
         cloth=terrain,
@@ -87,19 +55,14 @@ if __name__ == "__main__":
         point_fraction=0.1,
     )
 
-    # # visualize clusters and full pc
-    # full_cloud = cloud.clone()
-    # o3d.visualization.draw_geometries(
-    #     [c["cloud"].to_legacy() for c in clusters] + [cloud.to_legacy()]
-    # )
-
-    # # FIT TREES
-    # trees: List[Tree] = []
-    # for cluster in clusters:
-    #     tree = Tree(id=cluster["info"]["id"])
-    #     tree.add_cluster(cluster)
-    #     tree.reconstruct()
-    #     trees.append(tree)
+    # FIT TREES
+    trees: List[Tree] = []
+    for cluster in clusters:
+        tree = Tree(id=cluster["info"]["id"])
+        cluster["info"]["T_sensor2map"] = np.eye(4)
+        tree.add_cluster(cluster)
+        tree.reconstruct()
+        trees.append(tree)
 
     # # SAVE CLUSTER TO DISK
     # dir = "/home/ori/git/realtime-trees/single_trees/clustering_map"
@@ -117,16 +80,24 @@ if __name__ == "__main__":
     #         pickle.dump(cluster, file)
 
     # # PLOT TREE MESHES
-    # for tree, points in zip(
-    #     trees, [clusters["cloud"].point.positions.numpy() for clusters in clusters]
-    # ):
+    # viz_objects = []
+    # for tree in trees:
     #     verts, tris = tree.generate_mesh()
     #     if verts is None or tris is None:
     #         continue
 
-    #     plot_mesh(verts, tris, points)
-    #     print(
-    #         "Tree mesh has {} vertices and {} triangles".format(len(verts), len(tris))
+    #     mesh = o3d.geometry.TriangleMesh(
+    #         o3d.utility.Vector3dVector(verts),
+    #         o3d.utility.Vector3iVector(tris)
     #     )
+    #     mesh.compute_vertex_normals()
+    #     viz_objects.append(mesh)
 
-    print("Done!")
+    #     viz_objects.append(tree.clusters[0]["cloud"].to_legacy())
+    # o3d.visualization.draw_geometries(viz_objects)
+
+    # tree_manager = TreeManager()
+    # tree_manager.trees = trees
+    # with open("/home/ori/git/digiforest_drs/digiforest_analysis_ros/output/trees/logs/raw/tree_manager_aggregate.pkl", "wb") as file:
+    #     pickle.dump(tree_manager, file)
+    # print("Done!")
